87ecc1993227efcc89b3221d0c3b62e7e3a3cac0
challabhaskarrao <charanchalla205@gmail.com>
2026-08-18 11:20:00 +0530

    feat: add soil health score evaluator

--- utils.js
+++ utils.js
@@ -2,3 +2,8 @@
 export function calculateNPKRatio(nitrogen, phosphorus, potassium) {
     return { N: nitrogen, P: phosphorus, K: potassium };
 }
+
+export function getSoilHealthScore(ph, moisture) {
+    if (ph >= 6.0 && ph <= 7.5 && moisture >= 40) return 'Optimal';
+    return 'Needs Attention';
+}
